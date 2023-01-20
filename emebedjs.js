744a60c19936c0ed88f3aa165380eeb2f27d701b
Amir Sandila <amirmureed9@gmail.com>
2023-01-20 16:17:20 +0500

    Initial commit

---
+++ emebedjs.js
@@ -0,0 +1,32 @@
+const searchParams = new URLSearchParams(window.location.search);
+const uid = searchParams.get("uid");
+console.log(uid);
+if (uid) {
+    document.cookie = `uid=${uid};path=/`;
+    console.log("Cookie set");
+}
+
+function getCookie(name) {
+    const value = "; " + document.cookie;
+    const parts = value.split("; " + name + "=");
+    if (parts.length === 2) {
+        return parts.pop().split(";").shift();
+    }
+}
+
+if (window.location.href.includes("order-received")) {
+    const uid = getCookie("uid");
+    if (uid) {
+        var xhr = new XMLHttpRequest();
+        var url = "http://localhost/wpdemo/wp-admin/admin-ajax.php?action=purchase_complete";
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                // unset Cookies.
+            }
+        };
+        var data = JSON.stringify({ action: "purchase_complete", uid: uid, message: "purchased" });
+        xhr.send(data);
+    }
+}
